44aa9bc36fcef6df7b3216fbea956e13c3254b33
Mudaser Syed <mudaser.syed@hotmail.com>
2018-08-18 18:16:47 +1000

    Code Comment and changes

--- Automation/Xero.TestAutomation/Common/Assertion.cs
+++ Automation/Xero.TestAutomation/Common/Assertion.cs
@@ -9,21 +9,31 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Xero.TestAutomation.Common
 {
+    /// <summary>
+    /// Custom Assertion class
+    /// </summary>
     public static class Assertion
     {
-        public static void AssertContains(string expectedSubstrings1, string expectedString2, IReadOnlyCollection<IWebElement> BankAccountDetail, StringComparison comparisonType = StringComparison.CurrentCulture)
+        /// <summary>
+        /// Assertion on element Text property
+        /// </summary>
+        /// <param name="expectedSubstrings1">expectedSubstrings1</param>
+        /// <param name="expectedSubstrings2">expectedSubstrings2</param>
+        /// <param name="elements">IWebElements to get Text property for expected values</param>
+        /// <param name="comparisonType">String Comparison Type</param>
+        public static void AssertContains(string expectedSubstrings1, string expectedSubstrings2, IReadOnlyCollection<IWebElement> elements, StringComparison comparisonType = StringComparison.CurrentCulture)
         {
             bool results = false;
-            foreach (IWebElement element in BankAccountDetail)
+            foreach (IWebElement element in elements)
             {
                 if (AssertContains(expectedSubstrings1, element.Text.ToString(), comparisonType) &&
-                    AssertContains(expectedString2, element.Text.ToString(), comparisonType))
+                    AssertContains(expectedSubstrings2, element.Text.ToString(), comparisonType))
                 {
                     results = true;
                     break;
                 }
             }
-            Assert.IsTrue(results, $"Account Name {expectedSubstrings1} and Account Number {expectedString2} does not exist");
+            Assert.IsTrue(results, $"Account Name {expectedSubstrings1} and Account Number {expectedSubstrings2} does not exist");
         }
 
         public static bool AssertContains(string expectedSubstring, string actualString, StringComparison comparisonType = StringComparison.CurrentCulture)
